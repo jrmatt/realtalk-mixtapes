e0d18c53f52f6d1dec7c1ab478af89b3c225d060
Julia Matthews <julia.renee.matthews@gmail.com>
2026-05-19 16:28:28 -0400

    adds audio files

---
+++ track.gd
@@ -0,0 +1,7 @@
+class_name Track
+extends Resource
+
+@export var id: String
+@export var speaker_names: Array[String] = []
+@export var frequencies: Array[String] = []
+@export var audio: AudioStream
