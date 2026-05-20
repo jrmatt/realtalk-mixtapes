3fdbdd080317ea0c95a82ba5e25614d488a6364c
Julia Matthews <julia.renee.matthews@gmail.com>
2026-05-20 16:01:16 -0400

    radio plays tracks of a new frequency each time dial is pressed

---
+++ track_player.gd
@@ -0,0 +1,37 @@
+extends AudioStreamPlayer
+
+var track_library = load("res://resources/library.tres")
+var tracks = track_library.tracks
+var all_frequencies = load("res://resources/frequencies.tres")
+var frequencies = all_frequencies.frequencies
+var i = -1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+func _on_dial_pressed() -> void:
+	var freqs = frequencies
+	i += 1
+	var new_freq = freqs[i]
+	print(new_freq)
+	
+	var freq_tracks = _filter_by_freq(tracks, new_freq)
+	print(freq_tracks)
+
+	for track in freq_tracks:
+		self.stream.add_stream(-1, track.audio, 1)
+		
+	play()
+	
+	
+func _filter_by_freq(tracks_to_filter, frequency):
+	var freq_tracks: Array = []
+	
+	for track in tracks_to_filter:
+		if frequency in track.frequencies:
+			freq_tracks.append(track)
+			
+	return freq_tracks
+	
