3227c2db63a27f5502ed848b8b23f28549057436
Julia Matthews <julia.renee.matthews@gmail.com>
2026-05-21 10:05:07 -0400

    frequencies cycle

--- track_player.gd
+++ track_player.gd
@@ -1,12 +1,9 @@
 extends AudioStreamPlayer
 
-var track_library = load("res://resources/library.tres")
-var tracks = track_library.tracks
 var all_frequencies = load("res://resources/frequencies.tres")
 var frequencies = all_frequencies.frequencies
 var i = -1
 signal current_freq(new_freq)
-var current_track = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,16 +12,11 @@ func _ready() -> void:
 
 func _on_dial_pressed() -> void:
 	var freqs = frequencies
-	i += 1
+	i = (i +1) % freqs.size()
 	var new_freq = freqs[i]
 	current_freq.emit(new_freq)
 	print(new_freq)
 	
-	var freq_tracks = _filter_by_freq(tracks, new_freq)
-	print(freq_tracks)
-
-	for track in freq_tracks:
-		self.stream.add_stream(-1, track.audio, 1)
 		
 	play()
 	
@@ -37,4 +29,3 @@ func _filter_by_freq(tracks_to_filter, frequency):
 			freq_tracks.append(track)
 			
 	return freq_tracks
-	
