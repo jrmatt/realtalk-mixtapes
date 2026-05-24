0ed6199c1a641f8e4193536f79fbd8fc6f4aa4d9
Julia Matthews <julia.renee.matthews@gmail.com>
2026-05-23 21:15:05 -0400

    changes tabs to spaces

--- save.gd
+++ save.gd
@@ -5,18 +5,18 @@ var stack: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+    pass
 
 
 func _save_mix() -> void:
-	stack.add_child(mix_to_save)
-	print("Added current mix: ", mix_to_save)
+    stack.add_child(mix_to_save)
+    print("Added current mix: ", mix_to_save)
 
 
 func _on_save_btn_pressed() -> void:
-	_save_mix()
-	queue_free()
+    _save_mix()
+    queue_free()
 
 
 func _on_discard_btn_pressed() -> void:
-	queue_free()
+    queue_free()
--- speaker_label.gd
+++ speaker_label.gd
@@ -3,24 +3,24 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+    pass
 
 
 #func _on_dial_playing_track(new_track: Variant) -> void:
-	#var speaker_array = new_track.speakers
-	#var speakers = ", ".join(speaker_array)
-	#self.text = speakers
+    #var speaker_array = new_track.speakers
+    #var speakers = ", ".join(speaker_array)
+    #self.text = speakers
 
 
 func _on_bounds_playing_track(new_track: Variant) -> void:
-	if new_track:
-		var speaker_array = new_track.speakers
-		var speakers = ", ".join(speaker_array)
-		self.text = speakers
-	else:
-		self.text = ""
+    if new_track:
+        var speaker_array = new_track.speakers
+        var speakers = ", ".join(speaker_array)
+        self.text = speakers
+    else:
+        self.text = ""
