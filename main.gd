aa0f20787c662dd2e207e949ce19060a7ad13523
Jeff Hussmann <jeff.hussmann@gmail.com>
2026-05-28 22:28:26 -0400

    makes d-pad trigger focus grab

--- main.gd
+++ main.gd
@@ -40,6 +40,7 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
     if event is InputEventJoypadMotion:
+    if event.is_action_pressed('ui_left') or event.is_action_pressed('ui_right') or event.is_action_pressed('ui_up') or event.is_action_pressed('ui_down'):
         var current_focus = get_viewport().gui_get_focus_owner()
         if not current_focus:
             $Stacks/EmptyTapes.get_child(1).grab_focus()
