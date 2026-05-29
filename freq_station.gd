a6e8e33525ff2673e70325bca3f79ab96788a32d
Julia Matthews <julia.renee.matthews@gmail.com>
2026-05-29 13:46:22 -0400

    removes old print statements

--- freq_station.gd
+++ freq_station.gd
@@ -13,7 +13,6 @@ var bar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    print("I'm playing: ", freq.frequency_name)
     tracks.shuffle()
     finished.connect(_play_next_track)
     
