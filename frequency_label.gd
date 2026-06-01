eba88775ca109e913ed77e0756fbafe877e83e3f
Julia Matthews <julia.renee.matthews@gmail.com>
2026-06-01 14:27:49 -0400

    reconnects freq and speaker signals to updated dial node

--- frequency_label.gd
+++ frequency_label.gd
@@ -1,6 +1,7 @@
 extends Label
 
-func _on_bounds_playing_freq(new_freq: Variant) -> void:
+
+func _on_dial_playing_freq(new_freq: Variant) -> void:
     if new_freq:
         self.text = new_freq + ": "
     else:
