d48016392fd79f4e38f98949bc026d021892ebef
Julia Matthews <julia.renee.matthews@gmail.com>
2026-06-03 13:00:56 -0400

    removes print debug

--- radio_label.gd
+++ radio_label.gd
@@ -4,8 +4,6 @@ extends ScrollContainer
 
 func _process(delta: float) -> void:
     scroll_horizontal += 1
-    
-    print($HBoxContainer/Label2.text)
-    
+        
     if scroll_horizontal > label.size.x:
         scroll_horizontal = 0
