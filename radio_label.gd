6b4972d71aad89fb41962e1e460bed60dff530a1
Julia Matthews <julia.renee.matthews@gmail.com>
2026-06-04 10:00:24 -0400

    adds new radio sprites

--- radio_label.gd
+++ radio_label.gd
@@ -3,7 +3,7 @@ extends ScrollContainer
 @onready var label = $HBoxContainer/Label
 
 func _process(delta: float) -> void:
-    scroll_horizontal += 1
+    scroll_horizontal += 2
         
     if scroll_horizontal > label.size.x:
         scroll_horizontal = 0
